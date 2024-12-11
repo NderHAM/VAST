77a05d0f81bbb91b7d1e0381d9a1cdd58b74b8ad
NderHAM <hughsonliam@gmail.com>
2024-12-12 00:10:45 +1000

    yes

---
+++ src/main/java/net/vast/VASTDataGenerator.java
@@ -0,0 +1,11 @@
+package net.vast;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VASTDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
