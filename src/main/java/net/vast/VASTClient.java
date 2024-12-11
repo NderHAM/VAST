e0117ea17acac32de586cca99b0a18b0494a893d
NderHAM <hughsonliam@gmail.com>
2024-12-12 00:33:28 +1000

    did some stuff by following the modding course

---
+++ src/main/java/net/vast/VASTClient.java
@@ -0,0 +1,10 @@
+package net.vast;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VASTClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
