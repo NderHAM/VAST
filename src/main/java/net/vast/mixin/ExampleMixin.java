09eb8123374eaa9b573e1c67d5f833fb57f2b074
NderHAM <hughsonliam@gmail.com>
2024-12-14 21:17:00 +1000

    changed testing dimension name to The Library did some testing with mixins (still don't know how to use them fully)

--- src/main/java/net/vast/mixin/ExampleMixin.java
+++ src/main/java/net/vast/mixin/ExampleMixin.java
@@ -1,6 +1,8 @@
 package net.vast.mixin;
 
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.block.AbstractBlock;
+import net.vast.VAST;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,5 +13,6 @@ public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "loadWorld")
 	private void init(CallbackInfo info) {
 		// This code is injected into the start of MinecraftServer.loadWorld()V
+		VAST.LOGGER.info("im testing smth");
 	}
 }
